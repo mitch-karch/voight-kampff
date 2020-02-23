b55ffa6043614ea8ff1371b8516cdb4c560e27d4
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2020-02-23 10:17:55 -0800

    Slightly better formatting for reddit error message

--- commands_library/reddit_helper.py
+++ commands_library/reddit_helper.py
@@ -26,8 +26,7 @@ def reddit_top3(req_sub):
 
     tops = data["data"]["children"]
     if len(tops) < 3:
-        return errorEmbedBuilder("[Subreddit:{0}]({1})"
-                                 "has less than 3 recent posts"
+        return errorEmbedBuilder("[/r/{0}]({1}) has less than 3 recent posts"
                                  .format(req_sub,
                                          "https://www.reddit.com/r/"+req_sub),
                                  "Reddit"
